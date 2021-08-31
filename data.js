2e7dc36349ad4cb8a03423b628675989cbd5c464
maxthestranger <thestrangermax@gmail.com>
2021-09-01 01:29:07 +0300

    express skeleton layout

---
+++ data.js
@@ -0,0 +1,46 @@
+const event = {
+  event_title: 'Beautiful People Festival 2021',
+  start_date_time: '4-September-2021,12:00',
+  end_date_time: '4-September-2021,22:00',
+  venue: 'Morden Park, London, United Kingdom',
+  age_restriction: '18+',
+  description:
+    "Beautiful People heads to London's Morden Park this September, once again showcasing a vibrant day of all things house, soul, R&B, garage and disco.",
+  Lineup: [
+    'Arrested Development',
+    'Basement Jaxx [DJ set]',
+    'David Rodigan',
+    'David Morales',
+    'Black Motion',
+    'Derrick Carter',
+    'Todd Edwards',
+    'Grandmaster Flash',
+  ],
+  cover_art_url: 'assets/images/events/beatiful_people.jpg',
+  partner: true,
+  tickets: [
+    {
+      ticket_name: 'General Admission Ticket',
+      ticket_id: '',
+      description:
+        'VIP Tickets include: PRIORITY ENTRY, VIP MUSIC TERRACE, COMFY SEATS, NICE TOILETS, VIP BAR',
+      price: '71.96',
+      quantity: '124',
+    },
+    {
+      ticket_name: 'VIP Upgrade from Standard',
+      ticket_id: '',
+      description:
+        'Upgrade Your Standard or VIP Entry Ticket: Upgrades must be redeemed in conjunction with a valid Standard or VIP Entry ticket',
+      price: '32.71',
+      quantity: '740',
+    },
+  ],
+  active: true,
+  verified: true,
+  location: {
+    lat: '56.90',
+    long: '75.98',
+  },
+  event_url: 'https://www.youtube.com/watch?v=89gNDXwQVM4',
+};
